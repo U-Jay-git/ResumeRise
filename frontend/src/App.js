9c5a945eed2d61d9b31e39b6cfddd8d6f0025757
billa-anshu <billaanshu0@gmail.com>
2025-10-22 22:33:28 +0530

    demo text based mock interview

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ResumeMatchPage from "./pages/ResumeMatchPage";
 import MockInterviewPage from "./pages/MockInterviewPage";
-
+import InterviewSetupPage from './pages/InterviewSetupPage';
+import InterviewReviewPage from "./pages/InterviewReviewPage.jsx"; 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/resume-match" element={<ResumeMatchPage />} />
+        <Route path="/resume-match" element={<ResumeMatchPage />} /> 
         <Route path="/mock-interview" element={<MockInterviewPage />} />
+        <Route path="/interview-setup" element={<InterviewSetupPage />} /> 
+        <Route path="/interview-review" element={<InterviewReviewPage />} />
       </Routes>
     </Router>
   );
